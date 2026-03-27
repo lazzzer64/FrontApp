b76fb15836243476e5298d56c8937f8198200f8b
lazzzer64 <lazzzer64@gmail.com>
2026-03-27 22:23:35 +0300

    feat: Добавить футер с тремя кнопками

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css'
 import {Header} from "./ui/Header.tsx";
-import {Footer} from "./ui/Footer.tsx";
+import Footer from "./ui/Footer.tsx";
 import {Main} from "./ui/Main.tsx";
+
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
