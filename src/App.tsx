ffe0304cbe9ac1678667c11de845024e4e6dd9fc
lazzzer64 <lazzzer64@gmail.com>
2026-03-27 21:30:41 +0300

    feat: Добавить Header с кнопками на профиль пользователя, полем поиска и кнопки для ссылки на меня

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import './App.css'
 import {Sidebar} from "./ui/Sidebar.tsx";
+import {Header} from "./ui/Header.tsx";
+import {Footer} from "./ui/Footer.tsx";
+import {Main} from "./ui/Main.tsx";
 
 
 function App() {
 
     return (
         <div>
-            {/*<Header/>*/}
-            {/*<Main/>*/}
+            <Header/>
+            <Main/>
             <Sidebar/>
-            {/*<Footer/>*/}
+            <Footer/>
         </div>
     )
 }
--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,7 +1,37 @@
+import './css/Header.css';
+import './css/Search.css';
+import {useEffect, useState} from "react";
+
 export function Header() {
+    const [placeholder, setPlaceholder] = useState('');
+
+    const samplesOfSearch = [
+        '🔍 Поиск', '🔍 Почта', '🔍 Никнейм', '🔍 Номер карты'
+    ];
+
+    let index = 0;
+
+    useEffect(() => {
+        setPlaceholder(samplesOfSearch[0]);
+        const interval = setInterval(() => {
+            setPlaceholder(samplesOfSearch[index % samplesOfSearch.length]);
+            index++;
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
-        <main>
-            <p>Hello World from Header</p>
-        </main>
-    )
+        <header className="header">
+            <button className="profile-button">&#128100;</button>
+
+            <input
+                type="text"
+                placeholder={placeholder}
+                className="search-input"
+            />
+
+            <button className="cta-button">Связаться</button>
+        </header>
+    );
 }
