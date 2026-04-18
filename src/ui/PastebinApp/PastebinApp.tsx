b107c76e88f383a727da4b2140b8318f08676e2f
lazzzer64 <lazzzer64@gmail.com>
2026-04-18 21:10:10 +0300

    feat(PastebinApp): Добавить возможность загрузки файла с бекенда и форматировании его в элементе

--- src/ui/PastebinApp/PastebinApp.tsx
+++ src/ui/PastebinApp/PastebinApp.tsx
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import TextField from "./TextField.tsx";
 import SaveButton from "./SaveButton.tsx";
 import '../css/PastebinApp.css';
+import ArchivePost from "./ArchivePost.tsx";
 
 export function PastebinApp() {
     const [text, setText] = useState('');
@@ -14,6 +15,7 @@ export function PastebinApp() {
         <main className="main">
             <TextField onChange={handleTextChange}/>
             <SaveButton content={text}/>
+            <ArchivePost />
         </main>
     )
 }
